32ebaea689639f44acbd6009f25146615852a0b2
Thế Nguyễn <thend03122004@gmail.com>
2026-03-24 14:23:38 +0700

    feat-deploy

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -78,8 +78,8 @@ export class UserService {
       );
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
